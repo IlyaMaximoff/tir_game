505cfea98d72f3dbcb502dfecd5841debdc4aca3
Ilya Maximoff <ilia.maximoff@gmail.com>
2025-06-17 12:25:58 +0300

    add cicle code

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pygame
 import random
-from pygame.examples.scrap_clipboard import screen
-from pygame.examples.sprite_texture import running
 
 pygame.init()
 SCREEN_WIDTH = 800
@@ -13,16 +11,26 @@ icon = pygame.image.load("images/img1.jpg")
 pygame.display.set_icon(icon)
 
 target_img = pygame.image.load("images/img2.jpg")
-target_width = 80
-target_height = 80
+target_width = 116
+target_height = 116
 target_x = random.randint(0,SCREEN_WIDTH - target_width)
 target_y = random.randint(0,SCREEN_HEIGHT - target_height)
 
-color = ((random.randint(0,255), (random.randint(0,255),(random.randint(0,255))
+color = random.randint(0,255), random.randint(0,255), random.randint(0,255)
 
 running = True #Переменная running позволяет контролировать игровой цикл. В этой переменной сначала хранится значение True, которое позволяет циклу while работать бесконечно. То есть, пока running равно True, игра будет продолжаться и выполнять свои команды. Отдельная переменная running используется чтобы в нужный момент остановить игру. Когда потребуется завершить игровой процесс необходимо изменить значение этой переменной на False. Как только это произойдет, цикл while прекратится, и игра остановится. Таким образом, вся игра управляется этим циклом, и с помощью переменной running и можно контролировать ее завершение.
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
+    screen.blit(target_img,(target_x,target_y))
+    pygame.display.update()
 
 pygame.quit()
